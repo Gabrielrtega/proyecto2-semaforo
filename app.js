acd31be2f87f92226ac12f6fcabf53785419b4a4
valeortiz5 <valeria.ortiz2026@aodc.superate.org>
2026-09-23 14:48:10 -0600

    feat: implementa cambio a luz verde

---
+++ app.js
@@ -0,0 +1,4 @@
+// Estudiante A activa paso verde
+document.getElementById('luz-roja').classList.remove('activo');
+document.getElementById('luz-verde').classList.add('activo');
+aviso.textContent = "ADELANTE - VÍA LIBRE";
