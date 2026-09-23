b74e9f7d5cadd7f1e581443a343e40609ba26a29
valeortiz5 <valeria.ortiz2026@aodc.superate.org>
2026-09-23 14:53:23 -0600

    feat: codigo completo

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//const btn = document.getElementById('btnCambio');
+const aviso = document.getElementById('aviso');
+btn.addEventListener('click', () => {
+ aviso.textContent = "Sistema de tránsito operativo";
+});
+
 // Estudiante A activa paso verde
 document.getElementById('luz-roja').classList.remove('activo');
 document.getElementById('luz-verde').classList.add('activo');
